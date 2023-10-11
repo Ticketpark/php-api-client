0937fc93b203a4606a1aa734563f4bcaf2017b82
Manuel Reinhard <manu@sprain.ch>
2023-10-11 14:33:19 +0200

    Update example

--- example.php
+++ example.php
@@ -15,19 +15,18 @@ $client->setUserCredentials('your@username.com', 'yourPassword');
 // $client->setRefreshToken('someRefreshToken');
 
 // 3. Execute the desired command
-$response = $client->get('/events/', array('maxResults' => 2));
+$response = $client->get('/events/', ['maxResults' => 2]);
 
 // 4. Handle the response
-//    It is an instance of Buzz\Message\Response
 if ($response->isSuccessful()) {
-    print "<strong>Request successful!</strong><br>";
-    $events = json_decode($response->getContent(), true);
+    print "Request successful!\n\n";
 
+    $events = $response->getContent();
     foreach($events as $event) {
-        print $event['name']."<br>";
+        print $event['name']."\n";
     }
 }
 
-// 5. Get the tokens and store them to use them again later on
+// 5. Recommended: Get the tokens and store them to use them again later on
 $myAccessToken  = $client->getAccessToken();
 $myRefreshToken = $client->getRefreshToken();
